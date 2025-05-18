189d4da8b845c753925f2c02c144147a43e6b055
Matouš Eibich <matous.eibich@seznam.cz>
2025-05-18 10:10:33 +0200

    burn it down, start over

--- run_cli.py
+++
@@ -1,6 +0,0 @@
-"""Run the CLI for MindBuddy."""
-
-from src.mindbuddy.cli import main
-
-if __name__ == "__main__":
-    main()
--- src/mindbuddy/__init__.py
+++
@@ -1,3 +0,0 @@
-"""MindBuddy - A privacy-focused journaling buddy."""
-
-__version__ = "0.1.0"
